fb6f413751999f623064eecb3cce6c36b5793948
Logunathan J <71871300+Logunathan007@users.noreply.github.com>
2025-05-24 18:01:34 +0000

    FIX Memory leak in friends service and remove bootstarp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FriendsService } from './friends.service';
 import { NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,12 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'FriendsListFE';
   friends: string[] = [];
   newFriendName = '';
   isLoading = false;
+  private destroy$ = new Subject<void>();
 
   constructor(private friendsService: FriendsService) {}
 
@@ -23,50 +25,61 @@ export class AppComponent implements OnInit {
     this.loadFriends();
   }
 
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
   loadFriends() {
     this.isLoading = true;
-    this.friendsService.getAllFriends().subscribe({
-      next: (data: any) => {
-        this.friends = data;
-        this.isLoading = false;
-      },
-      error: () => {
-        this.isLoading = false;
-      }
-    });
+    this.friendsService.getAllFriends()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (data: any) => {
+          this.friends = data;
+          this.isLoading = false;
+        },
+        error: () => {
+          this.isLoading = false;
+        }
+      });
   }
 
   addFriend() {
     if (!this.newFriendName.trim()) return;
     this.isLoading = true;
-    this.friendsService.addFriend(this.newFriendName).subscribe({
-      next: (res: any) => {
-        if(res?.Failed === false){
-          this.friends.push(this.newFriendName);
-          this.newFriendName = '';
-        } else {
-          console.log('Failed to add friend:', res || 'Unknown error');
+    this.friendsService.addFriend(this.newFriendName)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (res: any) => {
+          if(res?.Failed === false){
+            this.friends.push(this.newFriendName);
+            this.newFriendName = '';
+          } else {
+            console.log('Failed to add friend:', res || 'Unknown error');
+          }
+          this.isLoading = false;
+        },
+        error: () => {
+          this.isLoading = false;
         }
-        this.isLoading = false;
-      },
-      error: () => {
-        this.isLoading = false;
-      }
-    });
+      });
   }
 
   deleteFriend(name: string) {
     this.isLoading = true;
-    this.friendsService.deleteFriend(name).subscribe({
-      next: (res: any) => {
-        if(res?.Failed === false){
-          this.friends = this.friends.filter(friend => friend !== name);
+    this.friendsService.deleteFriend(name)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (res: any) => {
+          if(res?.Failed === false){
+            this.friends = this.friends.filter(friend => friend !== name);
+          }
+          this.isLoading = false;
+        },
+        error: () => {
+          this.isLoading = false;
         }
-        this.isLoading = false;
-      },
-      error: () => {
-        this.isLoading = false;
-      }
-    });
+      });
   }
 }
